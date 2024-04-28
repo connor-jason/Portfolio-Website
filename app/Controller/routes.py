bca70af0378dac0c7137d1c19e362a8551b5c9e6
connor-jason <cejason@wpi.edu>
2024-04-28 14:35:38 -0400

    Added some comments to most files

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -2,19 +2,22 @@ from flask import Blueprint
 from flask import render_template
 from config import Config
 
+# Create a blueprint for routes
 routes_blueprint = Blueprint("routes", __name__)
 routes_blueprint.template_folder = Config.TEMPLATE_FOLDER
 
-
+# Define the index route
 @routes_blueprint.route("/", methods=["GET"])
 @routes_blueprint.route("/index", methods=["GET"])
 def index():
-    return render_template("index.html",title="Home",)
+    return render_template("index.html", title="Home")
 
+# Define the about_me route
 @routes_blueprint.route("/aboutme")
 def about_me():
     return render_template("about_me.html", title="About Me")
 
+# Define the coding_journey route
 @routes_blueprint.route("/coding_journey", methods=["GET"])
 def coding_journey():
     return render_template("coding_journey.html", title="My Coding Journey")
