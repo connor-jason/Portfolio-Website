b6af1bfebee659b675e4b72f523f896f03b6a264
connor-jason <cejason@wpi.edu>
2024-05-05 14:28:48 -0400

    last commit before website works maybe

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const app = express();
 const path = require('path');
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Define a route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 // Define a route to serve the index.html file when /index is requested
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 // Define a route to serve the index.html file when /home is requested
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 // Start the server
